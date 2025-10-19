f239ac02e4c18dccfc6d5d38e477dda91770e1cf
Israel Costa <isckosta@live.com>
2025-10-18 22:02:40 -0300

    feat: add JWT, permissions and Swagger service providers to bootstrap config

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+    L5Swagger\L5SwaggerServiceProvider::class,
 ];
